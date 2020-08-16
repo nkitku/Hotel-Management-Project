403918d030db55c0faee68c7e06670e8e2879a4d
nkitku <nkitku@gmail.com>
2020-08-17 00:49:36 +0530

    fix: :fire: removed unused data from hotel.sql

--- hotel.sql
+++ hotel.sql
@@ -35,26 +35,6 @@ CREATE TABLE `bookingtable` (
   PRIMARY KEY (`book_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `bookingtable`
---
-
-/*!40000 ALTER TABLE `bookingtable` DISABLE KEYS */;
-INSERT INTO `bookingtable` (`book_id`,`room_no`,`date_fro`,`date_to`,`no_of_day`) VALUES 
- (1,'101','2012-01-10 00:00:00','2012-01-12 00:00:00',NULL),
- (2,'103','2012-01-07 00:00:00','2012-01-10 00:00:00',NULL),
- (3,'105','2012-01-11 00:00:00','2012-01-13 00:00:00',NULL),
- (4,'108','2012-01-08 00:00:00','2012-01-09 00:00:00',NULL),
- (5,'112','2012-01-11 00:00:00','2012-01-13 00:00:00',NULL),
- (6,'118','2012-01-11 00:00:00','2012-01-13 00:00:00',NULL),
- (7,'123','2012-01-07 00:00:00','2012-01-10 00:00:00',NULL),
- (8,'120','2012-01-08 00:00:00','2012-01-09 00:00:00',NULL),
- (9,'110','2012-01-08 00:00:00','2012-01-08 00:00:00',NULL),
- (10,'125','2012-01-09 00:00:00','2012-01-10 00:00:00',NULL),
- (11,'109','2012-01-09 00:00:00','2012-01-11 00:00:00',2),
- (12,'108','2012-01-09 00:00:00','2012-01-12 00:00:00',3);
-/*!40000 ALTER TABLE `bookingtable` ENABLE KEYS */;
-
 
 --
 -- Definition of table `custdetail`
@@ -80,17 +60,6 @@ CREATE TABLE `custdetail` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `custdetail`
---
-
-/*!40000 ALTER TABLE `custdetail` DISABLE KEYS */;
-INSERT INTO `custdetail` (`Cust_name`,`Cust_add`,`Cust_city`,`Cust_state`,`Cust_country`,`Cust_ph`,`Cust_proof`,`Cust_adult`,`Cust_child`,`Cust_mar_stat`,`Cust_id_no`,`Cust_nation`,`Cust_purpos`,`Cust_bill`,`book_id`) VALUES 
- ('Vijay','AFS 2','Jamnagar','Gujarat','Indian','0288253422','Passport','1','0','Single','LDKF-999238-LL09-6','Indian','Tour',1568,11),
- ('Vidit','ABC','Jamnagar','Gujarat','India','9898263882','Voter ID','1','0','Single','3947QWE','Indian','Offical',2352,12);
-/*!40000 ALTER TABLE `custdetail` ENABLE KEYS */;
-
-
---
 -- Definition of table `login`
 --
 
@@ -106,10 +75,7 @@ CREATE TABLE `login` (
 --
 
 /*!40000 ALTER TABLE `login` DISABLE KEYS */;
-INSERT INTO `login` (`userid`,`password`) VALUES 
- ('Ayush','AC123'),
- ('Agrima','AAC123'),
- ('vijay','v123');
+INSERT INTO `login` (`userid`,`password`) VALUES ('admin','admin');
 /*!40000 ALTER TABLE `login` ENABLE KEYS */;
 
 
